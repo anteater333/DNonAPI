9ee1665fdcfa30ac6ce54d949e7ff75882ca47ce
Ji-hoon, Lee <zx1056@naver.com>
2020-10-28 21:39:17 +0900

    Seperating hard-coded connection info

--- app/main/config.py
+++ app/main/config.py
@@ -1,24 +1,26 @@
-import os
+import os, json
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+with open('./secrets.json', 'r') as f:
+    conn_info = json.load(f)
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dnon_api')    # KEY, jwt에 사용
+    SECRET_KEY = conn_info['SECRET_KEY']    # KEY, jwt에 사용
     DEBUG = False
 
 
 class DevelopmentConfig(Config):            # 개발버전 설정
     DEBUG = True
     ## DB 정보들 ##
-    MONGO_URI = 'mongodb://127.0.0.1:27017/dnonDevDB'
+    MONGO_URI = conn_info['DB_URL']
 
 class TestingConfig(Config):                # 테스팅 설정
     DEBUG = True
     TESTING = True
     PRESERVE_CONTEXT_ON_EXCEPTION = False   # 예외 발생 시 컨텍스트 보존 여부
     ## DB 정보들 ##
-    MONGO_URI = 'mongodb://127.0.0.1:27017/flaskAPITest'
+    MONGO_URI = conn_info['DB_TEST_URL']
 
 class ProductionConfig(Config):             # 배포판 설정
     DEBUG = False
